5f47ea9cb7776e6a8d1c60db64323238564252b5
bhuvan21 <bhuvan.belur@westminster.org.uk>
2018-12-14 22:45:56 +0000

    added basic app support

--- beeos.py
+++ beeos.py
@@ -1,3 +1,5 @@
+import os
+os.environ['KIVY_IMAGE'] = 'sdl2'
 import kivy
 kivy.require('1.10.1')
 
@@ -12,7 +14,10 @@ from kivy.uix.carousel import Carousel
 from kivy.graphics import Line, Ellipse
 from kivy.uix.screenmanager import Screen, ScreenManager, RiseInTransition
 
+
+
 import datetime
+import importlib.util
 from copy import deepcopy
 
 from device_info import get_password
@@ -87,7 +92,6 @@ class RestingLockScreen(Screen):
         Clock.schedule_interval(self.update_datetime, 1)
 
     def update_datetime(self, dt):
-        print(datetime.date.today())
         self.children[0].children[2].text = datetime.datetime.now().strftime("%H:%M")
         self.children[0].children[1].text = datetime.datetime.now().strftime("%A %-d %B")
 
@@ -110,6 +114,57 @@ class HomeScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add_widget(Image(source="images/Wallpapers/defaultHomeWallpaper.png"))
+        self.names = [app for app in os.listdir(os.getcwd()+"/apps/") if app[0] != "."]
+        self.paths = [os.getcwd() + "/apps/" + app for app in self.names]
+        self.apps = []
+        
+            
+            
+    def ready(self, parent):
+        for name, path in zip(self.names, self.paths):
+            app = {"name": name, "path": path}
+            spec = importlib.util.spec_from_file_location(name, path + "/" + name + ".py")
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            app["module"] = module
+            app["sm"] = module.get_app()
+            app["icon"] = path + "/" + module.get_icon()
+            self.apps.append(app)
+            screen = Screen(name=name)
+            screen.add_widget(app["sm"])
+
+            parent.add_widget(screen)
+        
+        self.realparent = parent
+
+        x = 42.5
+        y = 600
+        
+        for app in self.apps:
+            if x > 400:
+                x = 42.5
+                y -= 145
+
+            
+
+            button = Button(background_color=[0, 0, 0, 0],
+                        size_hint=[None, None],
+                        pos=[42.5, 600],
+                        text=app["name"],
+                        color=[0, 0, 0, 0])
+            img = Image(source=app["icon"], pos=(42.5-240+50, 600-400+50))
+            app["button"] = [button, img]
+            button.bind(on_press=self.openapp)
+            self.add_widget(button)
+            self.add_widget(img)
+            
+    def openapp(self, instance):
+        self.realparent.current = instance.text
+
+
+
+        
+        
 
 controller = ScreenManager()
 
@@ -121,16 +176,11 @@ lock_screen = LockScreen(name="Lock")
 lock_screen.children[0].children[0].children[0].switch_to_home = home_screen
 controller.add_widget(lock_screen)
 controller.add_widget(HomeScreen(name="Home"))
-
+controller.get_screen("Home").ready(controller)
 
 class BEEOSApp(App):
     def build(self):
         return controller
-    def on_touch_down(self, touch):
-        print(touch)
-
-
-
 
 if __name__ == '__main__':
     app = BEEOSApp()
---
+++ helperbee.py
@@ -0,0 +1,23 @@
+import os
+
+class HelperBEE:
+    def __init__(self):
+        pass
+    
+    def get_accelerometer():
+        pass
+
+    def get_potentiometer():
+        pass
+    
+    def get_buttons():
+        pass
+
+    def get_gestures():
+        pass
+    
+    def get_temperature():
+        pass
+    
+    def get_path():
+        return os.getcwd() + "/apps/"
