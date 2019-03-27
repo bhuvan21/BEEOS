2eb3c03b0584c8fbd83f37964d09de0bfd0dd955
bhuvan21 <bhuvan.belur@westminster.org.uk>
2019-03-27 21:22:20 +0000

    threading

--- SensorBEE.py
+++ SensorBEE.py
@@ -1,5 +1,6 @@
 import serial
-
+import threading
+import queue
 
 class SensorBEE():
     def __init__(self, dev_node="ttyACM0"):
@@ -17,4 +18,63 @@ class SensorBEE():
             total += read
         
         return total
+    
+    def get_split_values(self):
+        return [x for x in self.get_raw().split(":") if x != ""]
+    
+    def get_option_button(self):
+        return self.get_split_values()[0]
+    
+    def get_home_button(self):
+        return self.get_split_values()[2]
+    
+    def get_back_button(self):
+        return self.get_split_values()[1]
+    
+    def get_volume_slider(self):
+        return self.get_split_values()[3]
 
+    def get_temperature(self):
+        return self.get_split_values()[4]
+    
+    def get_raw_accel_x(self):
+        return self.get_split_values()[5]
+
+    def get_raw_accel_y(self):
+        return self.get_split_values()[6]
+
+    def get_raw_accel_z(self):
+        return self.get_split_values()[7]
+
+    def get_accel_x(self):
+        return self.get_split_values()[8]
+
+    def get_accel_y(self):
+        return self.get_split_values()[9]
+
+    def get_accel_z(self):
+        return self.get_split_values()[10]
+
+    def get_proximity(self):
+        return self.get_split_values()[11]
+    
+class SmartSensorBee():
+    def __init__(self, dev_node="ttyACM0"):
+        self.sensorbee = SensorBEE(dev_node=dev_node)
+        self.queue = queue.Queue()
+        self.thread = threading.Thread(None, self.refresh)
+        self.thread.setDaemon(True)
+        self.thread.start()
+
+    def refresh(self):
+        self.queue.put(self.sensorbee.get_split_values())
+        self.refresh()
+    
+    def get_split_values(self):
+        reading = 0
+        while True:
+            try:
+                reading = self.queue.get(block=False)
+            except queue.Empty:
+                break
+        return reading
